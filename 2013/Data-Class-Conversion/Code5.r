a4dfd0cc6aa0411d0c1e013555e7f9342b79e67d
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-11-20 11:30:43 +0800

    Create Code5.r

---
+++ 2013/Data-Class-Conversion/Code5.r
@@ -0,0 +1,13 @@
+data.mat <- as.matrix(data.f1)
+data.mat
+ 
+     var1 var2 var3
+[1,]    5   11   26
+[2,]    6   12   25
+[3,]    3   13   24
+[4,]    4   13   22
+[5,]    5   15   23
+ 
+class(data.mat)
+ 
+[1] "matrix"
