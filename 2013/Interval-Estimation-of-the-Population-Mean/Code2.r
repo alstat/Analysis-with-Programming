3d2775e3334432ba2b2144e13279909a69bfe462
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-11-20 20:46:13 +0800

    Create Code2.r

---
+++ 2013/Interval-Estimation-of-the-Population-Mean/Code2.r
@@ -0,0 +1,11 @@
+z.test(sam.scores, sigma.x = 6.8, conf.level = 0.95)$conf.int
+ 
+[1] 88.01916 92.65929
+attr(,"conf.level")
+[1] 0.95
+ 
+z.test(sam.scores, sigma.x = 6.8, conf.level = 0.99)$conf.int
+ 
+[1] 87.29014 93.38831
+attr(,"conf.level")
+[1] 0.99
