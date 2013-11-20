f2568dc82a040f4294cd3b86b01868c85485def8
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-11-20 11:25:38 +0800

    Delete Code5.r

--- 2013/Installing-Loading-and-Detaching-Packages/Code5.r
+++
@@ -1,8 +0,0 @@
-data.a <- c(3,4,5,2,1,3,6,5,4)
-matrix.a <- matrix(data.a, nrow = 3, ncol = 3, byrow = TRUE)
-matrix.a
- 
-     [,1] [,2] [,3]
-[1,]    3    4    5
-[2,]    2    1    3
-[3,]    6    5    4
