08a13d803f35b6465688b9b6bf24cba7c41efd2e
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-11-20 11:29:35 +0800

    Create Code4.r

---
+++ 2013/Data-Class-Conversion/Code4.r
@@ -0,0 +1,13 @@
+data.f1 <- data.frame(var1, var2, var3)
+data.f1
+ 
+  var1 var2 var3
+1    5   11   26
+2    6   12   25
+3    3   13   24
+4    4   13   22
+5    5   15   23
+ 
+class(data.f1)
+ 
+[1] "data.frame"
