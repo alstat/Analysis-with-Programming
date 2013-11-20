4217f54986be455152380838a6bca2a361482c8a
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-11-20 19:19:22 +0800

    Create Code1.r

---
+++ 2013/Matrix-Operations/Code1.r
@@ -0,0 +1,17 @@
+dat1 <- c(3,4,5,2,1,3,6,5,4)
+matrix1 <- matrix(dat1, nrow = 3, ncol = 3, byrow = TRUE)
+matrix1
+ 
+     [,1] [,2] [,3]
+[1,]    3    4    5
+[2,]    2    1    3
+[3,]    6    5    4
+ 
+dat2 <- c(6,7,5,4,5,8,7,6,6)
+matrix2 <- matrix(dat2, nrow = 3, ncol = 3, byrow = TRUE)
+matrix2
+ 
+     [,1] [,2] [,3]
+[1,]    6    7    5
+[2,]    4    5    8
+[3,]    7    6    6
