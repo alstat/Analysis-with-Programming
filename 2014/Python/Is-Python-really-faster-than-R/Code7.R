b9a7e50a1479c9bfc6239b766d678178e5afc499
Al-Ahmadgaid Asaad <alstated@gmail.com>
2014-02-14 09:45:50 +0800

    Create Code7.R

---
+++ 2014/Python/Is-Python-really-faster-than-R/Code7.R
@@ -0,0 +1,11 @@
+system.time(case2(rep = 100000))
+
+#Output
+   user  system elapsed 
+  0.296   0.016   0.315 
+
+system.time(case3(rep = 100000))
+
+#Output
+   user  system elapsed 
+  0.300   0.024   0.322 
