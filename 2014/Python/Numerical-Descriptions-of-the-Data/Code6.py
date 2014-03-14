260903644f20db9c009ee818a28a591fcd929df2
Al-Ahmadgaid Asaad <alstated@gmail.com>
2014-03-14 14:38:52 +0800

    Create Code6.py

---
+++ 2014/Python/Numerical-Descriptions-of-the-Data/Code6.py
@@ -0,0 +1,17 @@
+df.skew()
+
+# OUTPUT
+Abra       1.685130
+Apayao     0.663640
+Benguet    0.864243
+Ifugao    -0.125157
+Kalinga    0.388291
+
+df.kurt()
+
+# OUTPUT
+Abra       1.859803
+Apayao    -1.046026
+Benguet    1.592915
+Ifugao    -0.979704
+Kalinga   -1.335408
