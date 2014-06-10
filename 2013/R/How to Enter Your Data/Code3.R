c3c5e9b8d6daf0f4d90fc9ca71b258656cfe872d
alstat <alstated@gmail.com>
2014-06-10 17:47:52 +0800

    Added Codes

---
+++ 2013/R/How to Enter Your Data/Code3.R
@@ -0,0 +1,5 @@
+summary(vec1)
+
+# OUTPUT
+Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+0.10    0.20    0.30    0.34    0.50    0.60 
---
+++ 2013/R/How to Enter Your Data/Code4.R
@@ -0,0 +1,8 @@
+var(vec1) # Variance
+
+# OUTPUT
+[1] 0.043
+sd(vec1) # Standard Deviation
+
+# OUTPUT
+[1] 0.2073644
